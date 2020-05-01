1db3d931e0f5e3419c175c97fb83380be84f94a8
Juan Vasquez <juan.vasquez@mercadolibre.cl>
2020-05-01 14:36:54 -0400

    Initial Commit

---
+++ THFeedView/THFeedView.h
@@ -0,0 +1,19 @@
+//
+//  THFeedView.h
+//  THFeedView
+//
+//  Created by Juan Andres Vasquez Ferrer on 5/1/20.
+//  Copyright © 2020 Juan Andres Vasquez Ferrer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for THFeedView.
+FOUNDATION_EXPORT double THFeedViewVersionNumber;
+
+//! Project version string for THFeedView.
+FOUNDATION_EXPORT const unsigned char THFeedViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <THFeedView/PublicHeader.h>
+
+
---
+++ THFeedViewTests/THFeedViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  THFeedViewTests.swift
+//  THFeedViewTests
+//
+//  Created by Juan Andres Vasquez Ferrer on 5/1/20.
+//  Copyright © 2020 Juan Andres Vasquez Ferrer. All rights reserved.
+//
+
+import XCTest
+@testable import THFeedView
+
+class THFeedViewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
